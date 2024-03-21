7249c9d9aefe920e297dcd26d1de7a3dffd03459
gagankumar03 <gagankumar03@nagarro.com>
2024-03-21 14:51:20 +0530

    repository interface changed

--- backend/Jobs/src/main/java/com/jobsportal/Jobs/respository/UserRepository.java
+++ backend/Jobs/src/main/java/com/jobsportal/Jobs/respository/UserRepository.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Repository;
 import com.jobsportal.Jobs.entity.User;
 
 @Repository
-@EnableJpaRepositories
 public interface UserRepository extends MongoRepository<User, Integer> {
 	Optional<User> findByName(String username);
 }
